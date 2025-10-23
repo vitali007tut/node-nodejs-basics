5d2d68bb6b56e767375b52fb4be360827cab3eae
Rydkin Vitali <vrydkin@ibagroup.eu>
2025-10-23 11:16:19 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const copy = async () => {
-  // Write your code here
+    const sourceDir = path.join(process.cwd(), 'files');
+    const targetDir = path.join(process.cwd(), 'files_copy');
+
+    try {
+        await fs.access(sourceDir);
+
+        try {
+            await fs.access(targetDir);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+        }
+
+        await fs.cp(sourceDir, targetDir, { recursive: true });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
